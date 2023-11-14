65eb331594f091ff8f523f1fb61d5d222ccc218d
Kichan Lee <kichlee@c4r3s6.42seoul.kr>
2023-11-14 20:31:28 +0900

    1114

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <string>
-#include <stack>
 
-std::string toUpper(const std::string& str)
+
+std::string func_toupper(const std::string& str)
 {
 	std::string result = str;
 
-	for(int i = 0; i < str.length(); ++i)
+	for(int i = 0; i < (int) str.length(); ++i)
 		result[i] = std::toupper(result[i]);
 	return result;
 }
@@ -16,6 +15,6 @@ int main(int ac, char *av[])
 	if(ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for(int i = 1; i < ac; ++i)
-		std::cout << toUpper((std::string)av[i]);
+		std::cout << func_toupper((std::string)av[i]);
 	std::cout << std::endl;
 }
